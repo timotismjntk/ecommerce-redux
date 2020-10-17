356cf2211dd5c8708eaa3b4f16b8f6d4917e6e84
TimoDevs <30313199+timotismjntk@users.noreply.github.com>
2020-10-17 22:11:41 +0700

    Build Ecommerce with create react app

---
+++ build/precache-manifest.442136c88e7a1295a49eba774b2da156.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb214906adce90b7b513e6d1231f8aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b17a622cb461abc43514",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5a9191b1152e747c70bb",
+    "url": "/static/css/main.d00fac0e.chunk.css"
+  },
+  {
+    "revision": "b17a622cb461abc43514",
+    "url": "/static/js/2.662943a2.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.662943a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a9191b1152e747c70bb",
+    "url": "/static/js/main.97455586.chunk.js"
+  },
+  {
+    "revision": "c5a2fe7c5031baec7088",
+    "url": "/static/js/runtime-main.62a16bae.js"
+  },
+  {
+    "revision": "41960ea46f41c4635db2d9c8de8f17bc",
+    "url": "/static/media/1.41960ea4.png"
+  },
+  {
+    "revision": "f893fb2e56464608b016c122275fa383",
+    "url": "/static/media/2.f893fb2e.png"
+  },
+  {
+    "revision": "7d27646b48f916a11c0b22e27d477d05",
+    "url": "/static/media/3.7d27646b.png"
+  },
+  {
+    "revision": "e2d0bf82f4847c9eb2fad6f18799591c",
+    "url": "/static/media/4.e2d0bf82.png"
+  },
+  {
+    "revision": "3a6a1e6549af60f9784e00e3be86f17c",
+    "url": "/static/media/Search.3a6a1e65.svg"
+  },
+  {
+    "revision": "a8cdec8747c385bda06d12829e868482",
+    "url": "/static/media/Star.a8cdec87.svg"
+  },
+  {
+    "revision": "416a97bd9967c0b950bb6b487df6840c",
+    "url": "/static/media/StarGrey.416a97bd.svg"
+  },
+  {
+    "revision": "87754cec32e92fb2d38298e0c10456c3",
+    "url": "/static/media/bell.87754cec.svg"
+  },
+  {
+    "revision": "2a2dfc5f49134084908f146fb8e413a0",
+    "url": "/static/media/cart.2a2dfc5f.svg"
+  },
+  {
+    "revision": "7e018bad169b3cbe331482602bb6f48a",
+    "url": "/static/media/filter.7e018bad.svg"
+  },
+  {
+    "revision": "5c581b88d6e280e726b02201b2910ad2",
+    "url": "/static/media/kurang.5c581b88.svg"
+  },
+  {
+    "revision": "437fdbc5988bd17813940490279d8bed",
+    "url": "/static/media/logo.437fdbc5.svg"
+  },
+  {
+    "revision": "d503db415d56e335cc982ebb7c6adfd5",
+    "url": "/static/media/mail.d503db41.svg"
+  },
+  {
+    "revision": "395fccf9ecc123ba902d60c2806fb69d",
+    "url": "/static/media/map-icon.395fccf9.svg"
+  },
+  {
+    "revision": "4fbb44929ec761e96547d04c1db42f50",
+    "url": "/static/media/order-icon.4fbb4492.svg"
+  },
+  {
+    "revision": "c598edb9a475f7e21f3741c0bce5ac30",
+    "url": "/static/media/pen.c598edb9.svg"
+  },
+  {
+    "revision": "843803549c768cd9520f068961c518be",
+    "url": "/static/media/profil1.84380354.svg"
+  },
+  {
+    "revision": "2ced84108b0aab6e61d51c089474c456",
+    "url": "/static/media/profile.2ced8410.svg"
+  },
+  {
+    "revision": "8ccdec11327c66d0599daaf5743d715e",
+    "url": "/static/media/tambah.8ccdec11.svg"
+  },
+  {
+    "revision": "8ede59bebc79f933b6eb95dba2cbb61f",
+    "url": "/static/media/user-icon.8ede59be.svg"
+  }
+]);
